58af973dae71dc74c13a193be14a58ea1141c59a
Ahmed Mahmoud Mahmoud <62573937+AhmedSr12@users.noreply.github.com>
2021-06-14 17:29:04 +0200

    final code

--- GUI.py
+++ GUI.py
@@ -318,7 +318,7 @@ class gui:
                 index_x+=250
                 if ((k % 7 )==0) :
                     index_x = 20
-                    index_y+=350
+                    index_y+=250
                 k+=1
 
 
